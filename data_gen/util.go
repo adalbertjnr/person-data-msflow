38655817a7b872291562c7ef5bb2cd92efbdb2e5
Adalberto Junior <adalberto.gmu@gmail.com>
2024-02-20 22:03:57 -0300

    person mock data

---
+++ data_gen/util.go
@@ -0,0 +1,36 @@
+package main
+
+func personMockData() map[string]string {
+	return map[string]string{
+		"Emily Smith":      "Software Engineer",
+		"Jacob Johnson":    "Data Scientist",
+		"Olivia Williams":  "Product Manager",
+		"Michael Brown":    "UI/UX Designer",
+		"Ava Jones":        "Business Analyst",
+		"Ethan Davis":      "DevOps Engineer",
+		"Emma Taylor":      "QA Tester",
+		"Matthew Martinez": "Network Engineer",
+		"Sophia Anderson":  "Systems Analyst",
+		"William Wilson":   "Database Administrator",
+		"Isabella Thomas":  "Security Consultant",
+		"James White":      "Project Manager",
+		"Mia Harris":       "Technical Writer",
+		"Alexander Martin": "Scrum Master",
+		"Charlotte Garcia": "Solution Architect",
+		"Benjamin Miller":  "Data Engineer",
+		"Amelia Rodriguez": "Cloud Architect",
+		"Daniel Martinez":  "Full Stack Developer",
+		"Harper Lee":       "Cybersecurity Analyst",
+		"Joseph Moore":     "IT Consultant",
+		"Evelyn Jackson":   "Network Administrator",
+		"Samuel Thompson":  "Frontend Developer",
+		"Abigail Clark":    "Backend Developer",
+		"David Lewis":      "Systems Administrator",
+		"Elizabeth Scott":  "Mobile App Developer",
+		"Noah Walker":      "Machine Learning Engineer",
+		"Grace Hall":       "Database Developer",
+		"Lucas Young":      "Software Architect",
+		"Lily Hernandez":   "DevOps Specialist",
+		"Oliver King":      "Web Developer",
+	}
+}
