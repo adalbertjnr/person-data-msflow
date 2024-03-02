811a5806889d6667cece44bba08c89e0b8bfc712
Adalberto Junior <adalberto.gmu@gmail.com>
2024-03-02 09:14:50 -0300

    implementing both http && gprc server

--- aggregator/main.go
+++ aggregator/main.go
@@ -1,9 +1,36 @@
 package main
 
-import "log"
+import (
+	"context"
+	"log"
 
-const httpServerAddr = ":3001"
+	"github.com/adalbertjnr/ws-person/aggregator/client"
+	"github.com/adalbertjnr/ws-person/types"
+)
+
+const (
+	httpServerAddr = ":3001"
+	grpcServerAddr = ":4001"
+)
 
 func main() {
+	go func() {
+		if err := GRPCServer(grpcServerAddr); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	c, err := client.NewGRPCClientEndpoint(grpcServerAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := c.Aggregate(context.Background(), &types.AggregatePerson{
+		Id:    int32(2),
+		Name:  "foo",
+		Age:   int32(20),
+		Role:  "barRole",
+		Stage: "checking grpc client",
+	}); err != nil {
+		log.Fatal(err)
+	}
 	log.Fatal(HTTPServer(httpServerAddr))
 }
