b39b79d5feb91c9f2121841ced726ba8b5705c1d
Adalberto Junior <adalberto.gmu@gmail.com>
2024-03-03 12:27:06 -0300

    prometheus metrics to aggregator handlers

--- aggregator/http_server.go
+++ aggregator/http_server.go
@@ -5,15 +5,56 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/adalbertjnr/ws-person/types"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/sirupsen/logrus"
 )
 
+type HTTPMetrics struct {
+	aggregateCounter prometheus.Counter
+	aggregateLatency prometheus.Histogram
+}
+
+func NewHTTPMetrics(n string) *HTTPMetrics {
+	reqC := promauto.NewCounter(prometheus.CounterOpts{
+		Namespace: fmt.Sprintf("http_%s_request_counter", n),
+		Name:      "aggregator",
+	})
+	reqL := promauto.NewHistogram(prometheus.HistogramOpts{
+		Namespace: fmt.Sprintf("http_%s_request_latency", n),
+		Name:      "aggregator",
+	})
+	return &HTTPMetrics{
+		aggregateCounter: reqC,
+		aggregateLatency: reqL,
+	}
+}
+
+func (h *HTTPMetrics) instrumentHandlerWrapper(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer func(start time.Time) {
+			h.aggregateLatency.Observe(time.Since(start).Seconds())
+		}(time.Now())
+		h.aggregateCounter.Inc()
+		next(w, r)
+	}
+}
+
 func HTTPServer(httpServerAddr string) error {
-	aggSvc := NewDataStore()
-	http.HandleFunc("/aggregate", handleAggregate(aggSvc))
-	http.HandleFunc("/invoice", handleDataGetterByID(aggSvc))
+	var (
+		aggSvc = NewDataStore()
+		ag     = NewHTTPMetrics("agg_handler")
+		in     = NewHTTPMetrics("inv_handler")
+	)
+
+	http.HandleFunc("/aggregate", ag.instrumentHandlerWrapper(handleAggregate(aggSvc)))
+	http.HandleFunc("/invoice", in.instrumentHandlerWrapper(handleDataGetterByID(aggSvc)))
+	http.Handle("/metrics", promhttp.Handler())
+
 	fmt.Println("http aggregator server listening on port", httpServerAddr)
 	return http.ListenAndServe(httpServerAddr, nil)
 }
